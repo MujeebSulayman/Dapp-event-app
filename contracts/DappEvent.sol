babbbb018bcf3f380f4e0bc6b155ad9e6ea0371c
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-09-20 13:21:52 +0100

    Cursor

--- contracts/DappEvent.sol
+++ contracts/DappEvent.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
@@ -19,7 +18,7 @@ contract DappEvent is Ownable, ReentrancyGuard, ERC721 {
     string description;
     address owner;
     uint256 sales;
-    uint256 ticketCosts;
+    uint256 ticketCost;
     uint256 capacity;
     uint256 seats;
     uint256 startsAt;
@@ -48,12 +47,38 @@ contract DappEvent is Ownable, ReentrancyGuard, ERC721 {
   mapping(uint256 => TicketStruct[]) tickets;
   mapping(uint256 => bool) eventExists;
 
-    constructor (uint256 _pct) ERC721 ('Event X', 'ETX') {
+  constructor(uint256 _pct) ERC721('Event X', 'ETX') {
+    servicePct = _pct;
+  }
 
-    servicePct = _pct
-    }
+  function createEvent(
+    string memory title,
+    string memory description,
+    string memory imageUrl,
+    uint256 capacity,
+    uint256 ticketCost,
+    uint256 startsAt,
+    uint256 endsAt
+  ) public {
+    require(ticketCost > 0 ether, 'Ticket Cost must be greater than zero');
+    require(capacity > 0, 'Capacity must be greater than zero');
+    require(bytes(title).length > 0, 'Title must be greater than zero');
+    require(bytes(description).length > 0, 'Description must be greater than zero');
+    require(bytes(imageUrl).length > 0, 'Image Url can not be empty');
+    require(startsAt > 0, 'Start date must be greater than zero');
+    require(endsAt > 0 ether, 'End date must be greater than zero');
+ 
+    _totalEvents.increment();
+    EventStruct memory eventX;
 
-    function createEvent( string memory title, string memory description, string memory imageUrl, uint256 capacity, uint256 ticketCost, uint256 startsAt, uint256 endsAt)public {
-
-    }
+    eventX.id = _totalEvents.current();
+    eventX.title = title;
+    eventX.description = description;
+    eventX.imageUrl = imageUrl;
+    eventX.capacity = capacity;
+    eventX.ticketCost = ticketCost;
+    eventX.startsAt = startsAt;
+    eventX.endsAt = endsAt;
+    eventX.owner = msg.sender;
+  }
 }
