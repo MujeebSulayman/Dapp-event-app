6c97fff63ba196141f54f37d041fe7838902414d
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-09-30 15:24:38 +0100

    Last deployment

--- services/provider.tsx
+++ services/provider.tsx
@@ -13,14 +13,14 @@ import {
   coinbaseWallet,
   rainbowWallet,
 } from '@rainbow-me/rainbowkit/wallets'
-import { hardhat, sepolia } from 'wagmi/chains'
+import { hardhat } from 'wagmi/chains'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 
 const { chains, publicClient } = configureChains(
-  [sepolia, hardhat],
+   [hardhat],
   [
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string }),
     publicProvider()
