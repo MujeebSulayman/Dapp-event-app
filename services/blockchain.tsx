20895b16d6adc9b36288a51c69d3a6d9981e6329
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-09-30 10:34:36 +0100

    Reverse Head

--- services/blockchain.tsx
+++ services/blockchain.tsx
@@ -1,10 +1,9 @@
 import { ethers } from 'ethers'
-import contractAddress from '@/contracts/contractAddress.json'
-import DappEventXArtifact from '@/artifacts/contracts/DappEventX.sol/DappEventX.json'
+import address from '@/contracts/contractAddress.json'
+import abi from '@/artifacts/contracts/DappEventX.sol/DappEventX.json'
 import { EventParams, EventStruct, TicketStruct } from '@/utils/type.dt'
-
-const address = contractAddress.dappEventXContract
-const abi = DappEventXArtifact.abi
+import { globalActions } from '@/store/globalSlices'
+import { store } from '@/store'
 
 const toWei = (num: number) => ethers.parseEther(num.toString())
 const fromWei = (num: number) => ethers.formatEther(num)
@@ -12,29 +11,34 @@ const fromWei = (num: number) => ethers.formatEther(num)
 let ethereum: any
 let tx: any
 
-if (typeof window !== 'undefined') ethereum = window.ethereum
+if (typeof window !== 'undefined') ethereum = (window as any).ethereum
+const { setEvent, setTickets } = globalActions
 
 const getEthereumContracts = async () => {
   const accounts = await ethereum?.request?.({ method: 'eth_accounts' })
+
   if (accounts?.length > 0) {
     const provider = new ethers.BrowserProvider(ethereum)
     const signer = await provider.getSigner()
-    const contract = new ethers.Contract(address, abi, signer)
-    return contract
+    const contracts = new ethers.Contract(address.dappEventXContract, abi.abi, signer)
+
+    return contracts
   } else {
     const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL)
     const wallet = ethers.Wallet.createRandom()
     const signer = wallet.connect(provider)
-    const contract = new ethers.Contract(address, abi, signer)
-    return contract
+    const contracts = new ethers.Contract(address.dappEventXContract, abi.abi, signer)
+
+    return contracts
   }
 }
 
 const createEvent = async (event: EventParams): Promise<void> => {
   if (!ethereum) {
-    reportError('Please install wallet provider')
-    return Promise.reject(new Error('Please install wallet provider'))
+    reportError('Please install a browser provider')
+    return Promise.reject(new Error('Browser provider not installed'))
   }
+
   try {
     const contract = await getEthereumContracts()
     tx = await contract.createEvent(
@@ -47,6 +51,7 @@ const createEvent = async (event: EventParams): Promise<void> => {
       event.endsAt
     )
     await tx.wait()
+
     return Promise.resolve(tx)
   } catch (error) {
     reportError(error)
@@ -56,9 +61,10 @@ const createEvent = async (event: EventParams): Promise<void> => {
 
 const updateEvent = async (event: EventParams): Promise<void> => {
   if (!ethereum) {
-    reportError('Please install wallet provider')
-    return Promise.reject(new Error('Please install wallet provider'))
+    reportError('Please install a browser provider')
+    return Promise.reject(new Error('Browser provider not installed'))
   }
+
   try {
     const contract = await getEthereumContracts()
     tx = await contract.updateEvent(
@@ -72,6 +78,70 @@ const updateEvent = async (event: EventParams): Promise<void> => {
       event.endsAt
     )
     await tx.wait()
+
+    return Promise.resolve(tx)
+  } catch (error) {
+    reportError(error)
+    return Promise.reject(error)
+  }
+}
+
+const deleteEvent = async (eventId: number): Promise<void> => {
+  if (!ethereum) {
+    reportError('Please install a browser provider')
+    return Promise.reject(new Error('Browser provider not installed'))
+  }
+
+  try {
+    const contract = await getEthereumContracts()
+    tx = await contract.deleteEvent(eventId)
+    await tx.wait()
+
+    return Promise.resolve(tx)
+  } catch (error) {
+    reportError(error)
+    return Promise.reject(error)
+  }
+}
+
+const payout = async (eventId: number): Promise<void> => {
+  if (!ethereum) {
+    reportError('Please install a browser provider')
+    return Promise.reject(new Error('Browser provider not installed'))
+  }
+
+  try {
+    const contract = await getEthereumContracts()
+    tx = await contract.payout(eventId)
+    await tx.wait()
+
+    const eventData: EventStruct = await getEvent(eventId)
+    store.dispatch(setEvent(eventData))
+
+    return Promise.resolve(tx)
+  } catch (error) {
+    reportError(error)
+    return Promise.reject(error)
+  }
+}
+
+const buyTicket = async (event: EventStruct, tickets: number): Promise<void> => {
+  if (!ethereum) {
+    reportError('Please install a browser provider')
+    return Promise.reject(new Error('Browser provider not installed'))
+  }
+
+  try {
+    const contract = await getEthereumContracts()
+    tx = await contract.buyTickets(event.id, tickets, { value: toWei(tickets * event.ticketCost) })
+    await tx.wait()
+
+    const eventData: EventStruct = await getEvent(event.id)
+    store.dispatch(setEvent(eventData))
+
+    const ticketsData: TicketStruct[] = await getTickets(event.id)
+    store.dispatch(setTickets(ticketsData))
+
     return Promise.resolve(tx)
   } catch (error) {
     reportError(error)
@@ -85,16 +155,16 @@ const getEvents = async (): Promise<EventStruct[]> => {
   return structuredEvent(events)
 }
 
-const getEvent = async (eventId: number): Promise<EventStruct> => {
-  const contract = await getEthereumContracts()
-  const event = await contract.getSingleEvent(eventId)
-  return structuredEvent([event])[0]
-}
-
 const getMyEvents = async (): Promise<EventStruct[]> => {
   const contract = await getEthereumContracts()
   const events = await contract.getMyEvents()
   return structuredEvent(events)
+}
+
+const getEvent = async (eventId: number): Promise<EventStruct> => {
+  const contract = await getEthereumContracts()
+  const event = await contract.getSingleEvent(eventId)
+  return structuredEvent([event])[0]
 }
 
 const getTickets = async (eventId: number): Promise<TicketStruct[]> => {
@@ -102,19 +172,6 @@ const getTickets = async (eventId: number): Promise<TicketStruct[]> => {
   const tickets = await contract.getTickets(eventId)
   return structuredTicket(tickets)
 }
-
-const structuredTicket = (tickets: TicketStruct[]): TicketStruct[] =>
-  tickets
-    .map((ticket) => ({
-      id: Number(ticket.id),
-      eventId: Number(ticket.eventId),
-      owner: ticket.owner,
-      ticketCost: parseFloat(fromWei(ticket.ticketCost)),
-      timestamp: Number(ticket.timestamp),
-      refunded: ticket.refunded,
-      minted: ticket.minted,
-    }))
-    .sort((a, b) => b.timestamp - a.timestamp)
 
 const structuredEvent = (events: EventStruct[]): EventStruct[] =>
   events
@@ -138,4 +195,27 @@ const structuredEvent = (events: EventStruct[]): EventStruct[] =>
     }))
     .sort((a, b) => b.timestamp - a.timestamp)
 
-export { createEvent, updateEvent, getEvents, getEvent, getMyEvents, getTickets }
+const structuredTicket = (tickets: TicketStruct[]): TicketStruct[] =>
+  tickets
+    .map((ticket) => ({
+      id: Number(ticket.id),
+      eventId: Number(ticket.eventId),
+      owner: ticket.owner,
+      ticketCost: parseFloat(fromWei(ticket.ticketCost)),
+      timestamp: Number(ticket.timestamp),
+      refunded: ticket.refunded,
+      minted: ticket.minted,
+    }))
+    .sort((a, b) => b.timestamp - a.timestamp)
+
+export {
+  getEvents,
+  getMyEvents,
+  getEvent,
+  getTickets,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  buyTicket,
+  payout,
+}
